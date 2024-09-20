238f72a382af14282d4d37ad3277cc04010551ef
Default global <orderedset86@gmail.com>
2024-09-19 23:54:30 -0700

    Merge canonization systems, fix various crashing bugs

--- src/data/loadMachines.py
+++ src/data/loadMachines.py
@@ -5,61 +5,6 @@ import yaml
 from src.data.basicTypes import Ingredient, IngredientCollection, Recipe
 
 
-def standardizeMachineName(name):
-    replacements = {
-        'lgt': 'large gas turbine',
-
-        'lcr': 'large chemical reactor',
-
-        'ebf': 'electric blast furnace',
-        'blast furnace': 'electric blast furnace',
-
-        'xlgt': 'XL Turbo Gas Turbine',
-
-        'cal': 'circuit assembly line',
-
-        'fusion': 'fusion reactor',
-
-        'xlst': 'XL Turbo Steam Turbine',
-
-        'lst': 'large steam turbine',
-
-        'ico': 'industrial coke oven',
-
-        'exxonmobil': 'chemical plant',
-        'chem plant': 'chemical plant',
-
-        'tgs': 'tree growth simulator',
-
-        'utupu tanuri': 'industrial dehydrator',
-        'utupu-tanuri': 'industrial dehydrator',
-
-        'floation cell': 'floation cell regulator',
-
-        'isamill': 'isamill grinding machine',
-
-        'high current industrial arc furnace': 'industrial arc furnace',
-
-        'lpf': 'large processing factory',
-        
-        'industrial mixer': 'industrial mixing machine',
-
-        'industrial thermal centrifuge': 'large thermal refinery',
-
-        'industrial rock breaker': 'boldarnator',
-
-        'xl turbo steam turbine': 'XL Turbo Steam Turbine',
-        'xl steam turbine': 'XL Turbo Steam Turbine',
-        'xl turbo gas turbine': 'XL Turbo Gas Turbine',
-        'xl gas turbine': 'XL Turbo Gas Turbine',
-    }
-    
-    if name in replacements:
-        return replacements[name]
-    else:
-        return name
-
-
 def recipesFromConfig(project_name, project_folder='projects'):
     # Load config file
     CONFIG_FILE_PATH = Path(project_folder) / f'{project_name}'
@@ -78,7 +23,6 @@ def recipesFromConfig(project_name, project_folder='projects'):
             rec['dur'] *= 20
 
         machine_name = rec['m'].lower()
-        machine_name = standardizeMachineName(machine_name)
 
         recipes.append(
             Recipe(
